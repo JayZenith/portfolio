0a53b0fe2e00de16aeda7f116841eb803ba048fb
JayZenith <jayzenith248@gmail.com>
2024-08-16 17:39:40 -0700

    basic portfolio

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import './App.css';
+import ButtonMailto from "./ButtonMailto";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="App">
+          <div><p>Hello, my name is Jay Zenith.</p><br />
+          <p>I'm a developer.</p> <br></br>
+          <p>2024. I aint done yet.</p>
+          <br></br>
+          <Link to="https://github.com/JayZenith">Github</Link>
+          <br></br>
+          <br></br>
+          <Link to="https://x.com/JayZenith_">X</Link>
+          <br></br>
+          <br></br>
+          <ButtonMailto label="Email: jayzenith248@gmail.com" mailto="mailto:jayzenith248@gmail.com" />
+          </div>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
