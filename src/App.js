0583ca1ccf41b907f762d1a4e4d459b4b88c0a14
JayZenith <jayzenith248@gmail.com>
2024-09-12 01:26:13 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -11,18 +11,26 @@ function App() {
           <p>I'm a developer.</p> <br></br>
           <p>2024.</p>
           <br></br>
+          [
           <Link to="http://starfront.s3-website.us-east-2.amazonaws.com">
-          Days Before . . .
+           Days Before . . . 
           </Link>
+          ]
           <br></br>
           <br></br>
-          <Link to="https://github.com/JayZenith">Github</Link>
+          [
+          <Link to="https://github.com/JayZenith"> Github </Link>
+          ]
           <br></br>
           <br></br>
-          <Link to="https://x.com/JayZenith_">X</Link>
+          [
+          <Link to="https://x.com/JayZenith_"> X </Link>
+          ]
           <br></br>
           <br></br>
-          <ButtonMailto label="jayzenith248@gmail.com" mailto="mailto:jayzenith248@gmail.com" />
+          [
+          <ButtonMailto label=" jayzenith248@gmail.com " mailto="mailto:jayzenith248@gmail.com" />
+          ]
           </div>
         </div>
       </Router>
