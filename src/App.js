99eca4641861f2c82dd76fc542ae667ee9e4e780
JayZenith <jayzenith248@gmail.com>
2024-09-17 21:01:09 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -8,30 +8,32 @@ function App() {
     <>
       <Router>
         <div className="App">
-          <div><p>My name is Jay Zenith.</p><br />
-          <p>I'm a developer.</p> <br></br>
-          <p>2024.</p>
-          <br></br>
-          [
-          <Link to="http://starfront.s3-website.us-east-2.amazonaws.com">
-           Days Before . . . 
-          </Link>
-          ]
-          <br></br>
-          <br></br>
-          [
-          <Link to="https://github.com/JayZenith"> Github </Link>
-          ]
-          <br></br>
-          <br></br>
-          [
-          <Link to="https://x.com/JayZenith_"> X </Link>
-          ]
-          <br></br>
-          <br></br>
-          [
-          <ButtonMailto label="jayzenith248@gmail.com " mailto="mailto:jayzenith248@gmail.com" />
-          ]
+          <div className="center">
+            <div><p>My name is Jay Zenith.</p><br />
+            <p>I'm a developer.</p> <br></br>
+            <p>2024.</p>
+            <br></br>
+            
+            <Link to="http://starfront.s3-website.us-east-2.amazonaws.com">
+             Days Before . . .
+            </Link>
+            
+            <br></br>
+            <br></br>
+            
+            <Link to="https://github.com/JayZenith"> Github </Link>
+            
+            <br></br>
+            <br></br>
+            
+            <Link to="https://x.com/JayZenith_"> X </Link>
+            
+            <br></br>
+            <br></br>
+            
+            <ButtonMailto label="jayzenith248@gmail.com " mailto="mailto:jayzenith248@gmail.com" />
+            
+            </div>
           </div>
         </div>
       </Router>
