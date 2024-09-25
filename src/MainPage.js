805d311d22da01c8c89955828c0ebfee82552227
JayZenith <jayzenith248@gmail.com>
2024-09-24 20:00:34 -0700

    redid with tailwind css

--- src/MainPage.js
+++ src/MainPage.js
@@ -13,14 +13,14 @@ function MainPage() {
                 <img className='w-full h-full lg:h-full lg:w-full' src={mainImage}></img>
             </div>
             <div className='mt-10 w-full h-full lg:bg-white lg:w-1/2   lg:h-full   lg:pl-16 text-xl flex flex-col items-start'>
-                <p className='underline text-red-800 '>About Me</p>
+                <p className='border-dashed border-b-4  border-red-800 text-red-800 '>About Me</p>
                 <p className='mt-5 text-red-800 '>My name is Jay Zenith. I'm a developer interested in full stack development, machine learning, distributed systems, and the future
                     of computing. 
                 </p>
             </div>
             
             <div className='bg-white mt-10 w-full lg:w-1/4 lg:pl-16 text-xl flex flex-col items-start'>
-                <p className='underline text-red-800'>Links</p>
+                <p className='border-dashed border-b-4  border-red-800 text-red-800'>Links</p>
                 <ul className='mt-2 lg:mt-5 bg-white w-full'>
                     <li><a href="https://github.com/JayZenith" className='underline text-red-800 '>Github</a></li>
                     <li className='mt-3 text-red-800 '><a href="https://x.com/JayZenith_" className='underline'>X</a></li>
@@ -31,14 +31,14 @@ function MainPage() {
         </div>
         
     
-        <div className='pt-20 w-60 h-0 border-dashed border-b-4  border-red-800'></div>
+        <div className='pt-20 w-2/3 h-0 border-dashed border-b-4  border-red-800'></div>
         
         <div className='pt-10 lg:mt-10 w-64 lg:w-2/3  h-3/5 lg:h-1/5 bg-white flex flex-col lg:flex-row '>
             <div className='bg-white w-full h-full lg:w-1/4 lg:h-full mt-0 lg:mt-0'>
                 <img className='w-full h-full lg:h-full lg:w-full' src={redBox}></img>
             </div>
             <div className='mt-10 w-full h-full lg:bg-white lg:w-1/2   lg:h-full   lg:pl-16 text-xl flex flex-col items-start'>
-                <p className='underline text-red-800 '>Project 1</p>
+                <p className='border-dashed border-b-4  border-red-800 text-red-800 '>Project 1</p>
                 <p className='mt-5 text-red-800 '>
                     A review demo app. 
                 </p>
