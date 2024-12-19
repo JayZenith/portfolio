fde3af3744582240fcdec3ae621e060a8437b171
Jay Zenith <jayzenith248@gmail.com>
2024-12-18 18:03:35 -0800

    Added 3 new projects

--- src/MainPage.js
+++ src/MainPage.js
@@ -32,10 +32,64 @@ function MainPage() {
 
             </div>
             <div className='mt-10 w-full border-b-4 border-black text-xl font-bold'>The projects I care about</div>
-            <div className='mt-4'>I talk about my process of building a Regex Library in Rust <span className=''></span><span className='text-red-700 underline'><a className="" href="/regex">here</a></span></div>
-            
+
             
         </div>
+
+        <div className='mt-2 h-auto  lg:w-2/3 lg:h-72 lg:flex-row '>
+            <div className='lg:w-3/4 lg:pl-16 '>
+                <p className='text-lg'>Project 1</p>
+                <p className='mt-2 '>
+                    C++ Interpreter for the Lox Language. 
+                </p>
+           
+            </div>
+            <div className='mt-5 lg:mt-0 flex flex-col lg:items-center  items-center  '>
+                <li className='list-none  '><i class="bi bi-arrow-down font-bold text-2xl"></i></li>
+                <div className='w-32 h-32 lg:w-32 lg:h-32'>
+                    <a href="https://github.com/JayZenith/CppLoxInterpreter" className='underline text-black'><img className='w-full h-full lg:h-full lg:w-full 
+                          transition-transform duration-500 ease-in-out  hover:rotate-180'  src={redBox}></img></a>
+                </div>
+            </div>
+        </div>
+
+        <div className='mt-2 h-auto  lg:w-2/3 lg:h-72 lg:flex-row '>
+            <div className='lg:w-3/4 lg:pl-16 '>
+                <p className='text-lg'>Project 2</p>
+                <p className='mt-2 '>
+                    C++ compiler for my own language targeting x86 architecture. 
+                </p>
+           
+            </div>
+            <div className='mt-5 lg:mt-0 flex flex-col lg:items-center  items-center  '>
+                <li className='list-none  '><i class="bi bi-arrow-down font-bold text-2xl"></i></li>
+                <div className='w-32 h-32 lg:w-32 lg:h-32'>
+                    <a href="https://github.com/JayZenith/CompilerForTestLang" className='underline text-black'><img className='w-full h-full lg:h-full lg:w-full 
+                          transition-transform duration-500 ease-in-out  hover:rotate-180'  src={redBox}></img></a>
+                </div>
+            </div>
+        </div>
+
+
+        <div className='mt-2 h-auto  lg:w-2/3 lg:h-72 lg:flex-row '>
+            <div className='lg:w-3/4 lg:pl-16 '>
+                <p className='text-lg'>Project 3</p>
+                <p className='mt-2 '>
+                <div className='mt-4'>I talk about my process of building a Regex Library in Rust <span className=''></span><span className='text-red-700 underline'><a className="" href="/regex">here</a></span></div>
+                    Regex library written in Rust. 
+                </p>
+           
+            </div>
+            <div className='mt-5 lg:mt-0 flex flex-col lg:items-center  items-center  '>
+                <li className='list-none  '><i class="bi bi-arrow-down font-bold text-2xl"></i></li>
+                <div className='w-32 h-32 lg:w-32 lg:h-32'>
+                    <a href="https://github.com/JayZenith/RustRegexEngine" className='underline text-black'><img className='w-full h-full lg:h-full lg:w-full 
+                          transition-transform duration-500 ease-in-out  hover:rotate-180'  src={redBox}></img></a>
+                </div>
+            </div>
+        </div>
+
+
             
 
         <div className='mt-10 border-b-4 w-full lg:w-2/3 border-black text-xl'>Some full stack projects I made </div>
