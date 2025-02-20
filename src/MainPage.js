d27356f5dd410438306c79252a1bbd53c9b42a99
Jay Zenith <jayzenith248@gmail.com>
2025-02-19 18:12:27 -0800

    update

--- src/MainPage.js
+++ src/MainPage.js
@@ -20,18 +20,15 @@ function MainPage() {
                 under the hood and will document my journey here.
                 </p> 
                 <br />
-                <p className=''>
-                    I graduated with a Com Sci degree from UCSC 
-                    but am now on a mission to not be a mediocre dev.
-                </p>
+                
                 <br />
                 <p className=''>
-                    I am interested in compilers/interpreters, programming langauge theory, functional programming, and ML. 
+                    I am interested in compilers/interpreters, programming langauge theory, and ML. 
                 </p>
                 <br />
 
             </div>
-            <div className='mt-10 w-full border-b-4 border-black text-xl font-bold'>The projects I care about</div>
+            <div className='mt-10 w-full border-b-4 border-black text-xl font-bold'>Projects.</div>
 
             
         </div>
@@ -90,66 +87,6 @@ function MainPage() {
         </div>
 
 
-            
-
-        <div className='mt-10 border-b-4 w-full lg:w-2/3 border-black text-xl'>Some full stack projects I made </div>
-       
-        
-        <div className='mt-2 h-auto  lg:w-2/3 lg:h-72 lg:flex-row '>
-            <div className='lg:w-3/4 lg:pl-16 '>
-                <p className='text-lg'>Project 1</p>
-                <p className='mt-2 '>
-                    Review site made with React, ExpressJS, NodeJS, MySQL. 
-                </p>
-           
-            </div>
-            <div className='mt-5 lg:mt-0 flex flex-col lg:items-center  items-center  '>
-                <li className='list-none  '><i class="bi bi-arrow-down font-bold text-2xl"></i></li>
-                <div className='w-32 h-32 lg:w-32 lg:h-32'>
-                    <a href="https://github.com/JayZenith/Review-Application" className='underline text-black'><img className='w-full h-full lg:h-full lg:w-full 
-                          transition-transform duration-500 ease-in-out  hover:rotate-180'  src={redBox}></img></a>
-                </div>
-            </div>
-        </div>
-
-        {/*<p className='mt-10 w-2/3 h-auto  border-b-4 border-black'></p>*/}
-
-        <div className='mt-2 h-auto  lg:w-2/3 lg:h-72 lg:flex-row '>
-            <div className='lg:w-3/4 lg:pl-16 '>
-                <p className='text-lg'>Project 2</p>
-                <p className='mt-2 '>
-                    Images app made with React, ExpressJS, NodeJS, MongoDB.
-                </p>
-           
-            </div>
-            <div className='mt-10 lg:mt-0 flex flex-col lg:items-center  items-center  '>
-                <li className='list-none  '><i class="bi bi-arrow-down font-bold text-2xl"></i></li>
-                <div className='w-32 h-32 lg:w-32 lg:h-32'>
-                    <a href="https://github.com/JayZenith/TheCollage" className='underline text-black'><img className='w-full h-full lg:h-full lg:w-full 
-                          transition-transform duration-500 ease-in-out  hover:rotate-180'  src={redBox}></img></a>
-                </div>
-            </div>
-        </div>
-
-        {/*<p className='mt-10 w-2/3 border-b-4 border-black'></p>*/}
-
-
-        <div className='mt-2 h-auto  lg:w-2/3 lg:h-72 lg:flex-row '>
-            <div className='lg:w-3/4 lg:pl-16 '>
-                <p className='text-lg'>Project 3</p>
-                <p className='mt-2 '>
-                    Speech Recognition app made with React Native, Expo, NodeJS, ExpressJS, Google Speech-To-Text API. 
-                </p>
-           
-            </div>
-            <div className='mt-10 lg:mt-0 flex flex-col lg:items-center  items-center  '>
-                <li className='list-none  '><i class="bi bi-arrow-down font-bold text-2xl"></i></li>
-                <div className='w-32 h-32 lg:w-32 lg:h-32'>
-                    <a href="https://github.com/JayZenith/Speech-To-Text" className='underline text-black'><img className='w-full h-full lg:h-full lg:w-full 
-                          transition-transform duration-500 ease-in-out  hover:rotate-180'  src={redBox}></img></a>
-                </div>
-            </div>
-        </div>
 
 
         <p className='mt-10 w-2/3 border-b-4 border-black'></p>
