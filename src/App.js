84c9b4f0cb75541c9079b1ef066b226c00f99e93
Jay Zenith <jayzenith248@gmail.com>
2025-03-13 14:16:11 -0700

    new direction

--- src/App.js
+++ src/App.js
@@ -10,28 +10,9 @@ function App() {
   return (
     <>
       <Router>
-        <div className="basis">
-          {/*
-          <div className="bg-black h-20 w-full flex  items-center justify-center fixed top-0 ">
-            <ul className="flex flex-col justify-center text-white ">
-              <li className=""><a href="/" >
-                <div className="flex flex-row items-center justify-center text-2xl font-bold">Jay&nbsp;<div className="transition-transform duration-500 ease-in-out  hover:rotate-180 hover:text-red-800 ">Z</div>enith</div>
-                </a>
-              </li>
-              <div className="flex flex-row gap-5">
-                <li><a className="text-red-400 text-xl underline" href="https://twitter.com/jayz3nith">X</a></li>
-                <li><a className="text-red-400 text-xl underline" href="https://github.com/JayZenith">GitHub</a></li>
-              </div>
-              
-            </ul>
-          </div>
-          */}
-
+        <div className="AppBackground">
           <Routes>
-            <Route path="/" element={<MainPage />} />
- 
-
-          
+            <Route path="/" element={<MainPage />} />         
           </Routes>
         </div>
       </Router>
--- src/MainPage.js
+++ src/MainPage.js
@@ -9,27 +9,23 @@ function MainPage() {
     return (
         <div className="App">
           <header className="App-header">
-            <h1>JayZ3nith</h1>
+            <h1>JayZenith</h1>
             <div className='App-sub-header'>
                 <h2><a href="https://twitter.com/jayz3nith">X</a></h2>
                 <h2><a href="https://github.com/JayZenith">GitHub</a></h2>
             </div>
           </header>
           <section className="Content">
-            <h1>Intro. </h1>
-            <p>I'm Jay and I am fascinated with how computers work under the hood.</p>
-            <br />
-            <p>I'm interested in compilers/interpreters and machine learning.</p>
-            {/*
-            <div className="projects">
-              <h2>My Projects</h2>
-              <ul>
-                <li>Project 1</li>
-                <li>Project 2</li>
-                <li>Project 3</li>
-              </ul>
+            <h1 className="Content-Intro">Intro.</h1>
+            <div className='Intro'>
+              <p>I'm Jay and I am fascinated with how computers work under the hood and dedicate
+                my time in deconstructing abstractions.</p>
+              <br />
+              <p>I'm interested in compilers/interpreters, machine learning, program language theory, etc.
+                and the future of it all.</p>
             </div>
-            */}
+            
+            
           </section>
         </div>
       );
