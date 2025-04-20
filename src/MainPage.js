e2ffb8784af28aec47972b934ba0330b1bcdd534
JayZenith <jayzenith248@gmail.com>
2025-04-20 00:50:38 -0700

    update

--- src/MainPage.js
+++ src/MainPage.js
@@ -9,14 +9,14 @@ function MainPage() {
     return (
         <div className="App">
           <header className="App-header">
-            <h1>JayZenith</h1>
+            <h1>Jay Zenith</h1>
             <div className='App-sub-header'>
                 <h2><a href="https://twitter.com/jayz3nith">X</a></h2>
                 <h2><a href="https://github.com/JayZenith">GitHub</a></h2>
             </div>
           </header>
           <section className="Content">
-            <h1 className="Content-Intro">Intro.</h1>
+            <h2 className="Content-Intro">Intro</h2>
             <div className='Intro'>
               <p>I'm Jay and I am fascinated with how computers work under the hood and dedicate
                 my time in deconstructing abstractions.</p>
