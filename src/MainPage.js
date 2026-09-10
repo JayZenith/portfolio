cd664ede2cf444f5e7755b85e4df412273d5a8bb
JayZenith <jayzenith248@gmail.com>
2026-09-10 13:07:15 -0700

    Remove Hugging Face profile link

--- src/MainPage.js
+++ src/MainPage.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const profileLinks = [
   { label: 'github', href: 'https://github.com/JayZenith' },
-  { label: 'huggingface', href: 'https://huggingface.co/JayZenith' },
   { label: 'x', href: 'https://twitter.com/jayz3nith' },
 ];
 
